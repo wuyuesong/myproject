0e99a148249887921a3718c1ef4b0adaafa2aef9
wuyuesong <1377030423@qq.com>
2025-03-31 21:12:19 +0800

    feat: 店铺类型缓存

--- src/main/java/com/hmdp/service/IShopTypeService.java
+++ src/main/java/com/hmdp/service/IShopTypeService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 
 /**
@@ -12,5 +13,10 @@ import com.hmdp.entity.ShopType;
  * @since 2021-12-22
  */
 public interface IShopTypeService extends IService<ShopType> {
-
+    /**
+     * 获取商品类型列表
+     *
+     * @return {@link Result}
+     */
+    Result getTypeList();
 }
